bd93b1b41068d16061d8c85426cd63c52dc2143d
raffaelecapaldo <raffaelecpl@gmail.com>
2023-04-12 14:52:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+:<template>
+  <div>
+    <AppTitle />
+  </div>
+</template>
+
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  data () {
+    
+
+    return {}
+  },
+  components: {
+    AppTitle,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+:<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+
+    return {
+      title:"La mia prima app con Vite!" 
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
