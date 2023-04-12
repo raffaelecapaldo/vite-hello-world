58e0bd4d85ebaf8f7d164f573bb4720581bf9802
raffaelecapaldo <raffaelecpl@gmail.com>
2023-04-12 15:37:51 +0200

    Fixed style

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-5 mb-4 bg-body-tertiary rounded-3">
         <div class="container-fluid py-5">
-            <h1 class="display-5 fw-bold">Il mio primo componente Jumbotron!</h1>
+            <h1 class="display-5 fw-bold text-center">Il mio primo componente Jumbotron!</h1>
             <p class="text-center fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi atque repudiandae
                 incidunt explicabo porro corporis est repellat itaque, inventore tenetur possimus iusto perferendis iste
                 quasi quam, cumque dicta placeat dolore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
